fd555df7243729611079731dcedadb8f4e1069cb
jvitorfarias <joaovitorfarias4@gmail.com>
2025-05-12 21:32:32 -0300

    Correção atividade_dicionario_02

--- atividade_dicionario_02/main.py
+++ atividade_dicionario_02/main.py
@@ -14,93 +14,85 @@ Nome, CPF, Emai, Telefone, Data de Nascimento, Gênero.
 
 import os
 
-pessoas = []
+lista = []
 
 try:
     while True:
-        cadastrar = input("Deseja cadastrar uma nova pessoa? (s/n): ")
-        match cadastrar:
-            case 's':
-                # dicionário pessoa
-                pessoa = {}
-                
-                # Pede os dados da pessoa e adiciona ao dicionário pessoa
-                pessoa["nome"] = input("Informe o nome da pessoa: ")
-                pessoa["cpf"] = input("Informe o CPF da pessoa: ")
-                pessoa["email"] = input("Informe o email da pessoa: ")
-                pessoa["Telefone"] = input("Informe o telefone da pessoa: ")
-                pessoa["Data de Nascimento"] = input("Informe a data de nascimento da pessoa: ")
-                pessoa["Gênero"] = input("Informe o gênero da pessoa: ")
+        usuario = {}
+        # exibe o menu
+        print("Menu:")
+        print("1 - Cadastrar pessoa")
+        print("2 - Listar pessoas")
+        print("3 - Alterar dados de uma pessoa")
+        print("4 - Excluir pessoa")
+        print("5 - Sair")
 
-                # Adiciona a pessoa à lista de pessoas
-                pessoas.append(pessoa)
-                
+        opcao = input("\nEscolha uma das opções: ")
+        match opcao:
+            case '1':
+                # Pede os dados da pessoa e adiciona ao dicionário usuario
+                usuario["nome"] = input("Informe o nome da pessoa: ")
+                usuario["cpf"] = input("Informe o CPF da pessoa: ")
+                usuario["email"] = input("Informe o email da pessoa: ")
+                usuario["telefone"] = input("Informe o telefone da pessoa: ")
+                usuario["data_nascimento"] = input("Informe a data de nascimento da pessoa: ")
+                usuario["genero"] = input("Informe o gênero da pessoa: ")
+
+                lista.append(usuario)
                 os.system('cls')
-                print(f"{pessoa.get('nome')} cadastrada com sucesso!")
-                for pessoa in pessoas:
-                    print(f"\n{'-'*20}\n")
-                    for chave in pessoa:
-                        print(f"{chave.title()}: {pessoa.get(chave)}.")
-            case 'n':
-                break
+                print(f"{usuario.get('nome')} cadastrada com sucesso!")
+                continue
+            case '2':
+                os.system('cls')
+                # Listar pessoas
+                for i in range(len(lista)):
+                    print(f"\nPosição {i}:")
+                    for chave in lista[i]:
+                        print(f"{chave.title()}: {lista[i].get(chave)}")
+                print("\n")
+                continue
+            case '3':
+                # Alterar dados
+                os.system('cls')
+                posicao = int(input("Informe a posição da pessoa que deseja alterar: "))
+                if lista[posicao]:
+                    for chave in lista[posicao]:
+                        print(f"{chave.title()}: {lista[posicao].get(chave)}")
+                    print("\n")
+                    dado = input("Informe o nome do item que deseja alterar: ")
+                    if lista[posicao][dado]:
+                        lista[posicao][dado] = input(f"Informe o novo valor de {dado}: ")
+                        os.system('cls')
+                        print(f"{dado} alterada com sucesso!")
+                    else:
+                        os.system('cls')
+                        print(f"{dado} não existe.")
+                else:
+                    os.system('cls')
+                    print("Essa posição não existe.")
+                for i in range(len(lista)):
+                    print(f"\nPosição {i}:")
+                continue
+            case '4':
+                # Excluir pessoas
+                os.system('cls')
+                posicao = int(input("Informe a posição da pessoa que deseja excluir: "))
+                if lista[posicao]:
+                    del(lista[posicao])
+                    os.system('cls')
+                    print(f"Pessoa excluída com sucesso!")
+                else:
+                    os.system('cls')
+                    print("Não foi possível excluir a pessoa.")
+                continue
+            case '5':
+                os.system('cls')
+                # Sair do programa
+                print("Saindo do programa... Até a próxima!")
+                break   
             case _:
+                os.system('cls')
                 print("Opção inválida.")
                 continue
-        while True:
-            # Listar pessoas
-            for chave in pessoa:
-                print(f"{chave}: {pessoa.get(chave)}")
-
-                # alterar dados
-                prosseguir = input("Deseja alterar o dado de alguma pessoa? (s/n)")
-                match prosseguir:
-                    case "s":
-                        alterar = input("Informe o nome do item que deseja alterar: ")
-                        if alterar in pessoa:
-                            pessoa[alterar] = input("informe o novo valor da chave escolhida:")
-                            os.system("cls")
-                            continue
-                        else:
-                            os.system("cls")
-                            print(f"{alterar} não existe.")
-                        continue
-                    case "n":
-                        break
-                    case _:
-                        print("Opção inválida.")
-                        continue
-            # Excluir pessoas
-            excluir = input("Deseja excluir alguma pessoa? (s/n)")
-            match excluir:
-                case "s":
-                    nome = input("Informe o nome da pessoa que deseja excluir: ")
-                    for pessoa in pessoas:
-                        if pessoa["nome"] == nome:
-                            pessoas.pop(pessoa)
-                            os.system("cls")
-                            print(f"{nome} excluída com sucesso!")
-                            break
-                        else:
-                            print(f"{nome} não existe.")
-                            continue
-                case "n":
-                    break
-                case _:
-                    print("Opção inválida.")
-                    continue
-
-            # Sair do programa
-            sair = input("Deseja sair do programa? (s/n)")
-            match sair:
-                case "s":
-                    os.system("cls")
-                    print("Saindo do programa... Até a próxima!")
-                    break
-                case "n":
-                    os.system("cls")
-                    continue
-                case _:
-                    print("Opção inválida.")
-                    continue
 except Exception as e:
     print(f"Não foi possível cadastrar pessoa. {e}.")
